e67f81ad247f8b766a38edd95f3574e16661fc02
houzhenni <hzn2017@outlook.com>
2018-05-10 00:57:29 +0800

    add event for menubar
    add click/mouseover/mouseout event for menubar

--- topjs.js
+++ topjs.js
@@ -1,3 +1,33 @@
+$(".menuBarQuater").click(function(){
+  var that = this;
+  var menuChild_0 = this.children[0];
+  var menuChild_1 = this.children[1];
+  if(menuChild_1.style.display == "block"){
+  	that.style.backgroundColor = "transparent";
+  	menuChild_0.style.color = "white";
+    menuChild_1.style.display = "none";
+  }else{
+  	that.style.backgroundColor = "#FFAD86";
+  	menuChild_0.style.color = "black";
+    menuChild_1.style.display = "block";
+  }
+});
+$(".menuBarQuater").mouseover(function(){
+  var that = this;
+  var menuChild_0 = this.children[0];
+  var menuChild_1 = this.children[1];
+  that.style.backgroundColor = "#FFAD86";
+  menuChild_0.style.color = "black";
+  menuChild_1.style.display = "block";
+});
+$(".menuBarQuater").mouseout(function(){
+  var that = this;
+  var menuChild_0 = this.children[0];
+  var menuChild_1 = this.children[1];
+  that.style.backgroundColor = "transparent";
+  menuChild_0.style.color = "white";
+  menuChild_1.style.display = "none";
+});
 function toFixate(){
 	$("#float_menubar").attr("class", "menuBarFixated");
 	$("#float_middlePart").attr("class", "middlePartFixated");
